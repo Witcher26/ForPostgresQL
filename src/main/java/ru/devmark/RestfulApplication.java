020ae192e3cccc28dfbcf6306fe73725bcf23f9a
Witcher <[200~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~>
2022-06-23 17:52:24 +0300

    spring-boot-starter

---
+++ src/main/java/ru/devmark/RestfulApplication.java
@@ -0,0 +1,12 @@
+package ru.devmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ru.devmark")
+public class RestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApplication.class, args);
+    }
+}
