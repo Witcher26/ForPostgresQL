c18f87ad7d0775dbb6b8dd55b3a657b6820b8168
Witcher <[200~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~>
2022-06-24 16:37:30 +0300

    Обновлены зависимости. Не работает

--- src/main/java/RestfulApplication.java
+++ src/main/java/RestfulApplication.java
@@ -1,6 +1,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "ru.devmark")
 @SpringBootApplication(scanBasePackages = "ru.devmark")
 public class RestfulApplication {
 
